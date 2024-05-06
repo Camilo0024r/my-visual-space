b7c218a5cb87ffc40d1849b52fa649bc5e205b48
Camilo <duvan.sanabriam@gmail.com>
2024-05-06 18:28:17 -0500

    first commit

---
+++ src/app/components/template/header.tsx
@@ -0,0 +1,19 @@
+import logo from '/'
+
+function Header(){
+    return (
+        <>
+            <header>
+                <div>
+                <img src={logo} className="App-logo" alt="logo"/>
+                </div>
+                <div>
+                    
+                </div>
+            </header>
+        </>
+    );
+}
+
+
+export default Header;
